e68a19c3626e6c6ad794154c562f92598b7ab6e3
Glitch (codeming-bo) <none>
2019-10-19 14:14:51 +0000

    🎰🎡 Checkpoint ./komutlar/karantina.js:3765178/372

---
+++ komutlar/karantina.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+exports.run = (client, message, args) => { 
+  
+ if(args[0] === "kapalı") {
+   
+ }
+  
+  };
+exports.conf = {
+  enabled: true,  
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'saldırı',
+  description: 'taslak', 
+  usage: 'saldırı'
+};
