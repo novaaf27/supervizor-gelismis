67e67b7ef3e70e44a8c831c465607263f634ea3f
Glitch (codeming-bo) <none>
2019-10-20 10:47:02 +0000

    🌠😬 Checkpoint ./komutlar/onayla.js:3765178/36

--- komutlar/onayla.js
+++ komutlar/onayla.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => { 
-if(message.author.id !== "598176001037565960") if(message.author.id !== "419214688061227009") return message.reply('`yeterli yetkiye sahip değilsin.`')
+if(message.author.id !== "481121978426589184") if(message.author.id !== "419214688061227009") return message.reply('`yeterli yetkiye sahip değilsin.`')
 let kullanıcı = message.mentions.users.first()
 let komut = args.slice(1).join(' ');
 let embed3 = new Discord.RichEmbed()
