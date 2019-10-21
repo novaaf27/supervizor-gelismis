97921caa43f2d2da12337c44af07019fe2f14c0d
Glitch (codeming-bo) <none>
2019-10-21 15:18:33 +0000

    📊🏣 Checkpoint ./komutlar/paylaşımcı.js:3765178/925

--- komutlar/paylaşımcı.js
+++ komutlar/paylaşımcı.js
@@ -3,11 +3,21 @@ const db = require('quick.db')
 exports.run = async(client, message, args) => { 
   
   let kontrol = new Discord.RichEmbed()
-  .setTitle('<a:google:635846373052383243> Yetnkiniz Kontrol Ediliyor')
+  .setTitle('<a:google:635846373052383243> Yetkiniz Kontrol Ediliyor')
+  .setColor('BLUE')
   .setDescription('Sunucu veri tabanında yetkiniz kontrol ediliyor lütfen bekleyiniz..')
   
 message.channel.send(kontrol).then(x => {
-
+ setTimeout(() => {
+if(message.author.id !== "598176001037565960") {
+  
+    let kontrol = new Discord.RichEmbed()
+  .setTitle('<a:iptal:626445972620443648> Yetki Hatası')
+  .setDescription('Sunucu Veri Tabanında paylaşımcı eklebilmeyeniz için gerekli iznininizi bulamadım.')
+  .setColor('RED')
+    x.edit(kontrol) 
+}
+    }, 10000)
 })
   
   };
