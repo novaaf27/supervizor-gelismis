bd8ac4b710a9f4a7047f1d6c5837482e08871476
Glitch (codeming-bo) <none>
2019-10-21 17:05:13 +0000

    👓🐆 Checkpoint ./komutlar/paylaşımcı-paylaşım.js:3765178/2655

--- komutlar/paylaşımcı-paylaşım.js
+++ komutlar/paylaşımcı-paylaşım.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
 const talkedRecently = new Set();
-exports.run = (client, message, args) => { 
+exports.run = async(client, message, args) => { 
  
+  let kontrol3 = await db.fetch(`paylasimci_${message.member.id}`)   
+  
+  
+  
        if (talkedRecently.has(message.author.id)) {
       message.delete();
        return message.channel.send('Bu komut güvenlik nedeniyle saate 1 kullanılabilir.');
@@ -15,7 +19,43 @@ exports.run = (client, message, args) => {
         }, 7200000);
     }
   
+    let kontrol = new Discord.RichEmbed()
+  .setTitle('<a:google:635846373052383243> Yetkiniz Kontrol Ediliyor')
+  .setColor('BLUE')
+  .setDescription('Sunucu veri tabanında yetkiniz kontrol ediliyor lütfen bekleyiniz..')
   
+message.channel.send(kontrol).then(x => {
+ setTimeout(() => {
+if(!kontrol3) {
+  
+    let kontrol = new Discord.RichEmbed()
+  .setTitle('<a:iptal:626445972620443648> Yetki Hatası')
+  .setDescription('Sunucu Veri Tabanında paylaşımcı eklebilmeyeniz için gerekli iznininizi bulamadım.')
+  .setColor('RED')
+    x.edit(kontrol) 
+return
+}
+
+   
+   
+   let kontrol2 = new Discord.RichEmbed()
+  .setTitle('<a:basarl:626445944258560012> Yetki Kontrolü')
+  .setDescription('Yetki onaylandı! Lütfen botun sizden isteklerini gerçekleştirerek kod paylaşacağınız kanalı açın..')
+  .setColor('GREEN')
+    x.edit(kontrol2)   
+
+   
+ }, 10000)
+
+let katagori = new Discord.Rich.Embed()
+.setTitle('Katagori Seçimi')
+.setDescription('Lütfen aşağıdaki numaralara göre kod açmak istediğiniz kanalın numarasını bulunduğunuz kanala gönderin.')
+.addBlankField()
+.addField('Katagoriler:', '**1** => **javascript (s+)** \n **2** => **javascript (+)** \n **3** => **kod paylaşımları** \n **4** => bot, alt yapı** \n **5** => **html paylaşımları**')
+
+
+
+})
   };
 exports.conf = {
   enabled: true,  
