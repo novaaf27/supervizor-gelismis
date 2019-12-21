352c15e0d5f1e43d708201cd649e82a379f0d985
Glitch (codeming-bo) <none>
2019-12-21 20:33:50 +0000

    🏀🤗 Checkpoint ./ayarlar.json:9304233/432 ./events/ready.js:9304233/1849

--- events/ready.js
+++ events/ready.js
@@ -3,24 +3,22 @@ const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
+var prefix = ayarlar.prefix;
+
 module.exports = client => {
-
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-   var oyun = [
-
-        "Boş Alt Yapı!"
-     
-     
-
-       
- 
-   ];
-  setInterval(function() {
+var oyun = [
+         
+    ];
+    setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/rammus53");  //reklam değil xd
-        }, 2 * 3000);
-}
+        }, 5000);
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`•⍫ Swerve• Register System`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
